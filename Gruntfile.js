4d23c1088b5e6ffa70d12092c60fd83aa101ff2d
Álvaro Arranz <aarranz@fi.upm.es>
2015-01-13 23:14:14 +0100

    Fix grunt build settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,77 +1,102 @@
-/* 
- * @copyright 2014 CoNWeT Lab., Universidad Politécnica de Madrid
+/*   
+ * @copyright 2014-2015 CoNWeT Lab., Universidad Politécnica de Madrid
  * @license Apache v2 (http://www.apache.org/licenses/)
  */
 
+/*jshint node:true*/
+
 module.exports = function(grunt) {
 
-  'use strict';
+    'use strict';
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    banner: ' * @version <%= pkg.version %>\n' +
-            ' * \n' +
-            ' * @copyright 2014 <%= pkg.author %>\n' +
-            ' * @license <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
-            ' */',
+        banner: ' * @version <%= pkg.version %>\n' +
+                ' * \n' +
+                ' * @copyright 2014-2015 <%= pkg.author %>\n' +
+                ' * @license <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+                ' */',
 
-    compress: {
-      widget: {
-        options: {
-          mode: 'zip',
-          archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %>-dev.wgt'
+        bower: {
+            install: {
+                options: {
+                    layout: function(type, component, source) {
+                        console.log(type);
+                        return type;
+                    },
+                    targetDir: './src/lib'
+                }
+            }
         },
-        files: [
-          {expand: true, src: ['config.xml', 'index.html', 'js/**/*', 'css/**/*', 'images/**/*'], cwd: 'src'},
-          {expand: true, src: ['js/adapter.js'], dest: 'lib', cwd: 'src/bower_components/adapter.js/samples/web'},
-          {expand: true, src: ['css/bootstrap.min.css', 'js/bootstrap.min.js'], dest:'lib', cwd: 'src/bower_components/bootstrap/dist'},
-          {expand: true, src: ['ekko-lightbox.min.css'], dest:'lib/css', cwd: 'src/bower_components/ekko-lightbox/dist'},
-          {expand: true, src: ['ekko-lightbox.min.js'], dest:'lib/js', cwd: 'src/bower_components/ekko-lightbox/dist'},
-          {expand: true, src: ['jquery.min.js'], dest:'lib/js', cwd: 'src/bower_components/jquery/dist'},
-          {expand: true, src: ['js/kurento-utils.js'], dest:'lib', cwd: 'src/bower_components/kurento-utils'},
-        ]
-      }
-    },
-    
-    jasmine: {
-      src: ['src/js/*.js'],
-      options: {
-        specs: 'src/test/js/*Spec.js',
-        helpers: ['src/test/helpers/*.js'],
-        vendor: ['node_modules/jquery/dist/jquery.js',
-                'src/lib/js/jquery.dataTables.js',
-                'node_modules/jasmine-jquery/lib/jasmine-jquery.js']
-      }
-    },
 
-    replace: {
-      version: {
-        src: ['src/config.xml'],
-        overwrite: true,
-        replacements: [{
-          from: /version=\"[0-9]+\.[0-9]+\.[0-9]+(-dev)?\"/g,
-          to: 'version="<%= pkg.version %>"'
-        }]
-      }
-    },
+        compress: {
+            widget: {
+                options: {
+                    mode: 'zip',
+                    archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %>-dev.wgt'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: [
+                            'css/**/*',
+                            'doc/**/*',
+                            'images/**/*',
+                            'js/**/*',
+                            'lib/**/*',
+                            'index.html',
+                            'config.xml'
+                        ]
+                    }
+                ]
+            }
+        },
+        
+        jasmine: {
+            src: ['src/js/*.js'],
+            options: {
+                specs: 'src/test/js/*Spec.js',
+                helpers: ['src/test/helpers/*.js'],
+                vendor: [
+                    'node_modules/jquery/dist/jquery.js',
+                    'src/lib/js/jquery.dataTables.js',
+                    'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
+                ]
+            }
+        },
 
-    clean: ['build'],
+        replace: {
+            version: {
+                overwrite: true,
+                src: ['src/config.xml'],
+                replacements: [{
+                    from: /version=\"[0-9]+\.[0-9]+\.[0-9]+(-dev)?\"/g,
+                    to: 'version="<%= pkg.version %>"'
+                }]
+            }
+        },
 
-    jshint: {
-      all: ['src/js/**/*', 'src/test/**/*', 'Gruntfile.js', '!src/test/fixtures/']
-    }
+        clean: ['build'],
 
-  });
+        jshint: {
+            all: ['src/js/**/*', 'src/test/**/*', 'Gruntfile.js', '!src/test/fixtures/']
+        }
 
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-text-replace');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-compress');
+    });
 
-  grunt.registerTask('zip', 'compress:widget');
-  grunt.registerTask('version', ['replace:version']);
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-  grunt.registerTask('default', ['version', 'zip']);
+    grunt.registerTask('default', [
+        'bower:install',
+        'replace:version',
+        'compress:widget'
+    ]);
+
 };
